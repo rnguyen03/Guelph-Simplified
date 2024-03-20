daa050a69b3cdb4cc8a223edd3111afa463157d8
Ryan Nguyen <ryanvannguyen@gmail.com>
2024-03-20 11:22:49 -0400

    Initial Commit

---
+++ Guelph Simplified/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guelph Simplified
+//
+//  Created by Ryan Nguyen on 2024-03-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
